62b2e90a7b38168fbc9c1725b9db3ef13b8e59b9
shadpre <klavs@tranborg.eu>
2023-04-21 09:36:38 +0200

    pdf

--- src/TicketSystem/BLL/TicketManager.java
+++ src/TicketSystem/BLL/TicketManager.java
@@ -1,4 +1,35 @@
 package TicketSystem.BLL;
 
+import TicketSystem.BE.Event;
+import TicketSystem.BE.Ticket;
+import com.itextpdf.barcodes.Barcode2D;
+import com.itextpdf.kernel.pdf.*;
+import com.itextpdf.layout.Document;
+import com.itextpdf.layout.Style;
+import com.itextpdf.layout.element.Paragraph;
+
+import java.awt.*;
+import java.io.File;
+import java.io.IOException;
+import java.sql.SQLException;
+
 public class TicketManager {
+
+    public  static void printTicket(Event event, Ticket ticket) throws IOException {
+        PdfDocument pdf = new PdfDocument(new PdfWriter(ticket.getID()+".pdf"));
+        Document document = new Document(pdf);
+
+
+
+        document.add(new Paragraph(
+                event.getName() +"\r\n"+
+                        event.getDate() +"\r\n"+
+                        event.getEventStart()+"\r\n"
+        ));
+        document.add(new Paragraph(
+                "Billet ID: " + ticket.getID()+ "\r\n+"
+        ));
+        document.close();
+        Desktop.getDesktop().open(new File(ticket.getID()+".pdf"));
+    }
 }
